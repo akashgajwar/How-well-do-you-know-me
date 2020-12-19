0a6fd7957f5f2c04f7a742d4fcb02d3937903507
akashgajwar <75195665+akashgajwar@users.noreply.github.com>
2020-12-19 21:17:21 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,44 +1,39 @@
 var readlineSync = require('readline-sync');
 var chalk= require('chalk');
-var player = readlineSync.question(chalk.red("What is your Name? "));
-console.log(chalk.blue("Namaste "+ player));
-var answer = readlineSync.question(chalk.green("would you like to play polity quiz, "+ player+"? "));
+var userName = readlineSync.question(chalk.red("Your name please? "));
+console.log(chalk.green("welcome  "+ userName));
 
 var score = 0;
-if(answer ==="yes"){
-  function play(question,answer){
+
+function play(question,answer){
   var userAnswer = readlineSync.question(chalk.blue(question));
 
   if (userAnswer === answer){
-    console.log(chalk.green("you are right"));
+    console.log(chalk.red("You are right"));
     score++;
   }else{
-    console.log(chalk.green("Wrong Answer!"));
+    console.log(chalk.green("Better luck next time!"));
   }
 
   console.log(chalk.yellow("Your current score is " + score));
-  }
-
-  var highScore=[
-    {name:"Deepak", score:5},{name:"Kritesh",score:4},{name:"Aman",score:3},
-  ]
+}
   
 
 
-  var questions = [{question:"Q1.Who is PM of India ", answer:"Narendra Modi"},{
-  question:"Q2.Who is President of India ", answer:"Ram Nath Kovind"}, {question:"Q3.Who is Home Minister of India ", answer:"Amit Shah"},{question:"Q4.Who is Defence Minister of India ", answer:"RajNath Singh"},{question:"Q5.Who is Finance Minister of Inida ", answer:"Nirmala Sitharaman"}
-  ]
-  for (var i=0; i<questions.length; i++){
+var questions = [{question:"Q1.Who is my fav player? ", answer:"Dhoni"},{
+  question:"Q2.Who is my fav Actor ", answer:"Ranbir"},
+  {
+  question:"Q3.Which is my fav colour? ", answer:"Black"},
+  {
+  question:"Which is my fav fruit? ", answer:"Pineapple"}
+]
+for (var i=0; i<questions.length; i++){
   play(questions[i].question, questions[i].answer);
-  }
-
-  console.log(chalk.green("Your final score is " + score));
-
-  console.log(chalk.blue("highscore holder is " + highScore[0].name))
-  console.log(chalk.red("And the high score is " + highScore[0].score))
-
-  console.log(chalk.green("Thanks "+ player + " for participating!!"));
+}
+if(score === 4){
+  console.log(chalk.green("Youn know me well 👍 "))
 }
 else{
-  console.log(chalk.red("Hope to se you again " +player+"!"))
+  console.log(chalk.blue("You need to know me more"))
 }
+console.log(chalk.red("Welcome Again"));
